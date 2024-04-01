ce4f960f5f8be8fba18afc1fc6322a30a6efb53c
Felipe Alves <profelipealves@outlook.com>
2024-04-01 17:13:00 -0300

    chore: add flexbox and image warrior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+
 export default function Home(props) {
   return (
     <>
-      <main className=""></main>
+      <main className="flex h-screen w-screen gap-2 bg-[#152724]">
+        <div className="flex h-full w-full gap-2 bg-[#152724]">
+          <div className="h-full w-full bg-[#2C534A]">
+            <Image
+              src="/warrior.png"
+              alt="Logo"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </div>
+          <div className="flex h-full w-full flex-col justify-between bg-[#1D2F2C]">
+            <div className="w-full bg-[#2C534B]">
+              <h1 className="text-4xl font-bold text-white">Menu 1</h1>
+            </div>
+            <div className="w-full bg-[#2C534B]">
+              <h1 className="text-4xl font-bold text-white">Menu 2</h1>
+            </div>
+            <div className="w-full bg-[#2C534B]">
+              <h1 className="text-4xl font-bold text-white">Menu 3</h1>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-full w-full flex-col gap-2 bg-[#152724]">
+          <div className="h-full w-full bg-[#1D2F2C]">
+            <h1 className="text-4xl font-bold text-white">Mapa</h1>
+          </div>
+          <div className="h-full w-full bg-[#1D2F2C]">
+            <h1 className="text-4xl font-bold text-white">Atividades</h1>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
