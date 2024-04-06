617e6d9676075fa2af9d230d675c8c588815c4bd
Felipe Alves <profelipealves@outlook.com>
2024-04-06 16:49:18 -0300

    chore: finish interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,17 +97,104 @@ export default function Home(props) {
               </ul>
             </div>
           </div>
-          <div className="w-full bg-[#2C534B]">
-            <h1 className="text-4xl font-bold text-white">Menu 3</h1>
+          <div className="golden-gradient w-full bg-[#2C534B] p-1">
+            <div className="bg-[#2c534b]">
+              <h1
+                className="medieval-font p-2 text-2xl font-bold text-white"
+                onClick={() => setCollapsedSkills(!collapsedSkills)}
+              >
+                Skills
+              </h1>
+              <ul
+                className={`medieval-menu w-full overflow-y-auto ${collapsedSkills ? "hidden" : ""}`}
+              >
+                <li>
+                  <a>Apunhalhar 🗡️ [1]</a>
+                </li>
+                <li>
+                  <a>Saraivada de flechas 🏹 [2]</a>
+                </li>
+                <li>
+                  <a>Punhal venenoso 🗡️ [3]</a>
+                </li>
+                <li>
+                  <a>Furtividade 🗡️ [4]</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex w-full grow flex-col justify-end gap-1 bg-[#152724] pb-4">
+            <h1 className="medieval-font bg-[#2c534b] p-2 text-2xl font-bold text-white">
+              Dados
+            </h1>
+            <div className="text-md medieval-font rounded-md bg-red-600 text-center text-white">
+              100/100
+            </div>
+            <div className="text-md medieval-font rounded-md bg-blue-600 text-center text-white">
+              100/100
+            </div>
+            <div className="text-md medieval-font rounded-md bg-orange-600 text-center text-white">
+              100/100
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex h-full w-full flex-col gap-2 bg-[#152724]">
-        <div className="h-full w-full bg-[#1D2F2C]">
-          <h1 className="text-4xl font-bold text-white">Mapa</h1>
+      <div className="flex w-full flex-col gap-2 bg-[#152724]">
+        <div className="golden-gradient w-full rounded-lg">
+          <Image
+            src="/Valdheim.png"
+            alt="Logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-full w-full rounded-xl object-cover p-2"
+          />
         </div>
-        <div className="h-full w-full bg-[#1D2F2C]">
-          <h1 className="text-4xl font-bold text-white">Atividades</h1>
+        <div className="flex h-full w-full flex-row rounded-lg">
+          <div className="h-full w-full border-r-2 border-[#af9567]">
+            <h2 className="medieval-font p-2 text-2xl text-[#af9567]">
+              História
+            </h2>
+            <ul className="medieval-menu w-full overflow-y-auto">
+              <li>
+                <a>
+                  <div className="flex h-full w-full justify-around">
+                    <Image
+                      src="/coin.png"
+                      alt="Logo"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      className="h-10 w-10 object-cover"
+                    />
+                    <div className="m-auto">Encontre o Lobo cinzento [4]</div>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="h-full w-full">
+            <h2 className="medieval-font p-2 text-2xl text-[#af9567]">
+              Caçada
+            </h2>
+            <ul className="medieval-menu w-full overflow-y-auto">
+              <li>
+                <a>
+                  <div className="flex h-full w-full justify-around">
+                    <Image
+                      src="/coin-1.png"
+                      alt="Logo"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      className="h-10 w-10 object-cover"
+                    />
+                    <div className="m-auto">Mate 20 gremilins</div>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </main>
